f6646222090fad31cbbc1724fdc355820b84c4c1
jorovernier <jorovernier@gmail.com>
2023-12-05 11:46:11 -0700

    updated code

--- server/controller.js
+++ server/controller.js
@@ -1,19 +1,17 @@
 const movies = require('./db.json');
-let globalID = 11;
+let globalID = 11
 
 module.exports = {
     getMovies: (req, res) => {
         res.status(200).send(movies)
     },
     deleteMovie: (req, res) => {
-        // console.log(+req.params.id)
-        let index = movies.findIndex(elem => elem.id === +req.params.id);
-        movies.splice(index, 1);
-        res.status(200).send(movies);
+        let index = movies.findIndex(elem => elem.id === +req.params.id)
+        movies.splice(index, 1)
+        res.status(200).send(movies)
     },
     createMovie: (req, res) => {
-        // console.log(req.body)
-        const {title, rating, imageURL} = req.body;
+        const {title, rating, imageURL} = req.body
 
         // Visual Example of req.body
 
@@ -29,19 +27,19 @@ module.exports = {
             rating: +rating,
             imageURL
         }
-        movies.push(newMovie);
-        globalID++;
-        res.status(200).send(movies);
+        movies.push(newMovie)
+        globalID++
+        res.status(200).send(movies)
     },
     updateMovie: (req, res) => {
-        const {type} = req.body;
+        const {type} = req.body
         let index = movies.findIndex(elem => +elem.id === +req.params.id);
         if(type === 'minus' && movies[index].rating > 1){
-            movies[index].rating -= 1;
-            res.status(200).send(movies);
+            movies[index].rating -= 1
+            res.status(200).send(movies)
         } else if(type === 'plus' && movies[index].rating < 5){
-            movies[index].rating += 1;
-            res.status(200).send(movies);
+            movies[index].rating += 1
+            res.status(200).send(movies)
         } else {
             res.status(400).send('Invalid star rating!')
         }
